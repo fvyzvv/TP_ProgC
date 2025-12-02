7b48d96da1f3e86331fda412cb93061a8a2db14b
fvyzvv <feyza.bahtiyar@edu.esiee-it.fr>
2025-12-02 11:41:38 +0100

    Update operator.h

---
+++ TP4/src/operator.h
@@ -0,0 +1,13 @@
+#ifndef OPERATOR_H
+#define OPERATOR_H
+
+int addition(int a, int b);
+int soustraction(int a, int b);
+int produit(int a, int b);
+int quotient(int a, int b);
+int modulo(int a, int b);
+int et_bit(int a, int b);
+int ou_bit(int a, int b);
+int negation(int a);  // prend un seul argument
+
+#endif
