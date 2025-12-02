90f3854e6aa5cf569772a57901c31be9d0e620da
fvyzvv <feyza.bahtiyar@edu.esiee-it.fr>
2025-12-02 11:45:36 +0100

    Update main.c

--- TP4/src/main.c
+++ TP4/src/main.c
@@ -1,1 +1,29 @@
 
+#include <stdio.h>
+#include "operator.h"
+#include "fichier.h"
+#include "liste.h"
+
+void exercice41();
+void exercice42();
+void exercice47();
+
+int main() {
+    int choix;
+
+    printf("Quel exercice voulez-vous exécuter ?\n");
+    printf("1 : Exercice 4.1\n");
+    printf("2 : Exercice 4.2\n");
+    printf("3 : Exercice 4.7\n");
+    printf("> ");
+    scanf("%d", &choix);
+
+    switch (choix) {
+        case 1: exercice41(); break;
+        case 2: exercice42(); break;
+        case 3: exercice47(); break;
+        default: printf("Choix invalide.\n");
+    }
+
+    return 0;
+}
