bf9f8d7a719bab4b86d1be16f4f3b9c93894e30d
fvyzvv <feyza.bahtiyar@edu.esiee-it.fr>
2025-12-02 09:31:43 +0100

    Update bits.c

--- TP2/src/bits.c
+++ TP2/src/bits.c
@@ -1,17 +1,1 @@
-#include <stdio.h>
 
-int main() {
-    int a = 2;     
-    int b = 3;     
-    int resultat = 1; 
-
-    // Boucle pour calculer a^b
-    for (int i = 0; i < b; i++) {
-        resultat = resultat * a;
-    }
-
-    // Affichage du résultat
-    printf("%d élevé à la puissance %d = %d\n", a, b, resultat);
-
-    return 0;
-}
